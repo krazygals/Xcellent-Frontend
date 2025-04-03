5063ee721d04ee1e93cd394f79f439964cef28fe
Oriana Olmstead <orianaolmstead@Orianas-MacBook-Pro.local>
2025-04-03 12:28:15 -0600

    Fixed match-columns URL for deployed site

--- src/components/PasteColumns.js
+++ src/components/PasteColumns.js
@@ -33,8 +33,8 @@ const handleClean = () => {
 
   const handleTestMatch = async () => {
     try {
-      const response = await axios.post('/api/match-columns', {
-        user_columns: cleanedColumns
+        const response = await axios.post('https://xcellent.onrender.com/api/match-columns', {
+            user_columns: cleanedColumns
       });
       setMatches(response.data.suggested_matches);
     } catch (error) {
